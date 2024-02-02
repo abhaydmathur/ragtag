d32c7f3e2400399d2a965e7e002e3e6b08d63e3d
Paul  Houssel <paul.houssel@telecom-sudparis.eu>
2024-02-02 16:10:28 +0100

    include new cli args

--- prepare.sh
+++ prepare.sh
@@ -4,12 +4,9 @@ set -e
 ROOT=$(dirname $0)
 cd $ROOT
 
-
 module purge
 module load pytorch-gpu/py3/2.1.1 
 cd qallm
-python3 -m venv .venv
-.venv/bin/pip install --upgrade pip
-.venv/bin/pip install --no-cache-dir -r requirements.txt
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
 pip install .
--- run.sh
+++ run.sh
@@ -15,5 +15,4 @@ cd $ROOT
 module purge
 module load pytorch-gpu/py3/2.1.1 
 cd qallm
-source .venv/bin/activate
 qallm -c "$QUESTIONS" -o "$OUTPUT"
